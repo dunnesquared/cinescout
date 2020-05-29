9e5a92fd77e34d8a8e861aaee2ebc6d4c2910866
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-05-29 15:45:44 -0400

    Add hash functions to User model

--- cinescout/models.py
+++ cinescout/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from cinescout import db
 
 class User(db.Model):
@@ -7,7 +9,14 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return f"{self.username}, {self.email}"
+
 
 

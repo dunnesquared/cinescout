eb985081a3f17e81bde7fe368f6d35fd43a8da37
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-05-30 17:55:27 -0400

    Ensure app.db in root is database used for this app

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 # Run as "source setup.sh"
 export FLASK_APP=app.py
 export FLASK_DEBUG=True
+
+# Use this to ensure that database being used is project's app.db
+# Comment out this line if you are migrating the app to another db
+# (e.g. Postgre) such that you will be setting DATABASE_URL variable to
+# whatever resource you're migrating too.
+unset DATABASE_URL

c7cacf4c32ffb94418ec03c511347bc7c60a4a1b
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-06-03 16:58:47 -0400

    Add classes to encapsulate and info-hid movie review data fetching from views

--- cinescout/movies.py
+++ cinescout/movies.py
@@ -21,12 +21,12 @@ class Movie:
 
 
     @classmethod
-    def get_movie_list_by_title(cls):
+    def get_movie_list_by_title(cls, title):
         """Returns list of movies."""
         pass
 
     @classmethod
-    def get_movie_info_by_id(cls):
+    def get_movie_info_by_id(cls, id):
         """Builds Movie object with id to search external api db."""
         pass
 
@@ -41,6 +41,7 @@ class Movie:
 
     def __repr__(self):
         pass
+
 
 class TmdbMovie(Movie):
 
@@ -82,7 +83,7 @@ class TmdbMovie(Movie):
 
     @classmethod
     def get_movie_info_by_id(cls, id):
-        """Get info from TMDB API  to create instance.
+        """Get info from TMDB API to create instance.
 
         Args:
             id: Integer representing TMDB movie id.
@@ -131,31 +132,169 @@ class TmdbMovie(Movie):
 
 
 
+class MovieReview:
+
+    def __init__(self, title=None, year=None, text=None):
+        self.title = title
+        self.year = year
+        self.text = text
+
+
+    @classmethod
+    def get_review_by_title_and_year(cls, title, year):
+        pass
+
+    def __str__(self):
+        return f"'{self.title}' ({self.year}): {self.text}"
 
 
 
-# movie_info = {
-#     'tmdb_id': tmdb_id,
-#     'title': tmdb_movie_data['title'],
-#     'opening_date': tmdb_movie_data['release_date'],
-#     'year': int(tmdb_movie_data['release_date'].split('-')[0].strip()),
-#     'overview': tmdb_movie_data['overview'],
-#     'poster_url_full': poster_url_full,
-#     'nyt_critics_pick': nyt_critics_pick,
-#     'nyt_summary_short': nyt_summary_short,
-#     'on_user_list': on_user_list,
-# }
-#
-#
-# class MovieReview:
-#     pass
-#
-#
-# class NytMovieReview(MovieReview):
-#     pass
+class NytMovieReview(MovieReview):
+
+    api_key = os.getenv('NYT_API_KEY')
+    delay = 3
+
+    def __init__(self, title=None, year=None, text=None, critics_pick=None):
+        MovieReview.__init__(self, title, year, text)
+        self.critics_pick = critics_pick
+
+
+    @classmethod
+    def get_review_by_title_and_year(cls, title, year):
+        # Setup return value
+        result = {'success': True,
+                  'status_code': 200,
+                  'message': None,
+                  'review': None}
+
+        # Get review info from NYT
+    	# Flag in case NYT review info already found
+        nyt_review_already_found = False
+
+        print(f"RELEASE YEAR = {year}")
+
+        opening_date_start = f"{year}-01-01"
+        opening_date_end = f"{year}-12-31"
+
+        res = requests.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json",
+                                params={"api-key": cls.api_key,
+                                        "opening-date": f"{opening_date_start};{opening_date_end}",
+                                        "query": title.strip()})
+
+        if res.status_code != 200:
+            result['success'] = False
+            result['status_code'] = res.status_code
+            return result
+
+        # Unpack review data
+        nyt_data = res.json()
+
+        # Check whether a review was written for a movie. Watch out for special cases.
+        if nyt_data["num_results"] == 0:
+            print("No NYT review found☹️")
+
+            nyt_status = "No review found."
+            nyt_critics_pick = "N/A"
+            nyt_summary_short = "N/A"
+
+            # Check case where year in NYT db and TMBD don't match.
+            print("Checking to see whether release year in TMDB and NYT don't match....")
+
+            print(f"Delaying next NYT API call by {NYT_API_DELAY} seconds...")
+            time.sleep(cls.delay)
+
+            print("Making second request to NYT with TMDB release year unspecified...")
+            res = requests.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json",
+                                    params={"api-key": cls.api_key,
+                                            "query": title.strip()})
+
+            if res.status_code != 200:
+                result['success'] = False
+                result['status_code'] = res.status_code
+                return result
+
+            print("Request successful. Counting number of reviews returned...")
+            nyt_data = res.json()
+
+            if nyt_data["num_results"] == 0:
+                print("There are really no reviews for this movie. Sorry😭")
+
+            if nyt_data["num_results"] == 1:
+                print("One review found. Hopefully this is it🙏🏻!")
+
+            if nyt_data["num_results"] > 1:
+                print("More than one review found🤔")
+                print("Using heuristic that NYT reviewed film at closest date AFTER TMDB's release date....")
+
+                print("Analyzing NYT review results...")
+
+                # Get critics pick data and summary short for review that has years closest to TMDBs
+                years = []
+
+                for result in nyt_data['results']:
+                    # Extract year
+                    print("Extacting NYT release or publcation year...", end="")
+                    nyt_date = result['opening_date'] if result['opening_date'] else result['publication_date']
+                    nyt_year = nyt_date.split('-')[0].strip()
+                    print(nyt_year)
+
+                    years.append(nyt_year)
+
+                print("Getting NYT year closest to TMDB year...", end="")
+                shortlist = []
+
+                for year in years:
+                    diff = int(year) - int(release_year)
+                    if diff > 0:
+                        shortlist.append(diff)
+
+                result_index = shortlist.index(min(shortlist))
+                print(years[result_index])
+
+                print("Getting review information. Hopefully right review picked🙏!")
+                nyt_critics_pick = nyt_data['results'][result_index]['critics_pick']
+                nyt_summary_short = nyt_data['results'][result_index]['summary_short']
+
+                # So we don't overwrite the found review results with the logic below
+                nyt_review_already_found = True
+
+
+        if nyt_data["num_results"] > 1 and not nyt_review_already_found:
+
+            nyt_status = "ERROR: More than one review found. Unable to choose."
+            nyt_critics_pick = "N/A"
+            nyt_summary_short = "N/A"
+            result['success'] = False
+
+        if  nyt_data["num_results"] == 1:
+            nyt_status = "OK"
+            nyt_critics_pick = nyt_data['results'][0]['critics_pick']
+            nyt_summary_short = nyt_data['results'][0]['summary_short']
+
+            if nyt_summary_short is not None:
+                if nyt_summary_short.strip() == "":
+                    nyt_summary_short = "No summary review provided."
+
+
+        print(f"NYT_STATUS: {nyt_status}")
+
+        # Build review object
+        review = cls(title=title, year=year, text=nyt_summary_short, critics_pick=nyt_critics_pick)
+        result['message'] = nyt_status
+        result['review'] = review
+
+        return result
+
+
+
 
 if __name__ == "__main__":
-    movie = TmdbMovie(123, "Cat Game", "1992", "1992-03-07", "Cat chases mouse.",
-                    "107", "/csrw/01")
-    print(movie)
-    print(movie.poster_rel_url)
+    # movie = TmdbMovie(123, "Cat Game", "1992", "1992-03-07", "Cat chases mouse.",
+    #                 "107", "/csrw/01")
+    # print(movie)
+    # print(movie.poster_full_url)
+    result = TmdbMovie.get_movie_info_by_id(id=88)
+    print(result['movie'])
+
+    result = NytMovieReview.get_review_by_title_and_year(title="Dirty Dancing", year="1987")
+    print(result['review'])

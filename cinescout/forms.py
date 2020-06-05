2ebcd7494ed7d0a86fe8c7055095941fc7a2ffd5
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-06-05 12:18:20 -0400

    Comment code associated with registration feature

--- cinescout/forms.py
+++ cinescout/forms.py
@@ -1,3 +1,5 @@
+"""Implements logic to render and validate web forms for cinescout features"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import EmailField
@@ -10,35 +12,55 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+
 class RegistrationForm(FlaskForm):
+    """Form for new users to register with site"""
+
+    # Class data
     username = StringField('Username',
                             validators=[DataRequired(),
                                         Regexp('^\w+$',
                                         message="Username must contain only alphanumeric or underscore characters.")
                             ])
+
     email = EmailField('Email', validators=[ DataRequired(), Email()])
 
     password = PasswordField('Password', validators=[DataRequired(),
                             EqualTo('password2', message="Passwords do not match."),
-                            Length(min=8, message="Password must be at least 8 characters long.")
+                            Length(min=8,
+                                  message="Password must be at least 8 characters long.")
                             ])
+
     password2 = PasswordField('Re-enter Password',
                               validators=[DataRequired()])
+
     submit = SubmitField('Register')
 
 
     def validate_username(self, username):
-        """Checks that username has not already been used"""
+        """Checks that username has not already been used.
+
+        Args:
+            username: String representing username of user.
+
+        Raises:
+            ValidationError: if username already in use.
+        """
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Username already taken. Please use another.')
 
     def validate_email(self, email):
         """Checks that user is not creating multiple accounts with same
-        email."""
+        email.
 
-        print(f"EMAIL={self.email.data}")
+        Args:
+            email: String representing user's email.
 
+        Raises:
+            ValidationError: if email already in use.
+
+        """
         user = User.query.filter_by(email=email.data).first()
 
         if user:
--- cinescout/views.py
+++ cinescout/views.py
@@ -34,11 +34,15 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-	"""Allows non-logged in users to register for account"""
+	"""Allows non-logged in users to register for account."""
+
+	# Logged in users shouldn't be allowed to register.
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
 
 	form = RegistrationForm()
+
+	# Ensure user-entered data is good. If so, register and take to login page!
 	if form.validate_on_submit():
 		user = User(username=form.username.data, email=form.email.data)
 		user.set_password(form.password.data)
@@ -47,6 +51,7 @@ def register():
 		flash(f'Welcome to cinescout, {form.username.data}! Now login to get started.')
 		return redirect(url_for('login'))
 
+	# When users GET the register page
 	return render_template('register.html', title='Register', form=form)
 
 

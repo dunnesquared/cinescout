8b3c19f1069bb9699e149e3e6160eb46848c0a04
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-06-23 15:09:57 -0400

    Ensure sample movies on users' lists have complete info

--- scripts/film_data.py
+++ scripts/film_data.py
@@ -113,8 +113,13 @@ if __name__ == "__main__":
 
         # Add movies to personal lists
         print("Adding movies to personal lists...")
-        film1 = FilmListItem(user_id=1, title="Wendy and Lucy", year=2008, tmdb_id=8942)
-        film2 =  FilmListItem(user_id=2, title="MacGruber", year=2010, tmdb_id=37931)
+        film1 = FilmListItem(user_id=1, title="Wendy and Lucy",
+                             year=2008, tmdb_id=8942,
+                             date="2008-05-22",
+                             original_title="Wendy and Lucy")
+        film2 =  FilmListItem(user_id=2, title="MacGruber",
+                              year=2010, tmdb_id=37931,
+                              date="2010-05-21", original_title="MacGruber")
         db.session.add(film1)
         db.session.add(film2)
         db.session.commit()

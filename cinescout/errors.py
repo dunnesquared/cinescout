2820a9f8ff06b3c6c97f1bdcff2b73c857abb95d
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-06-25 11:10:45 -0400

    Fix method names; comment

--- cinescout/errors.py
+++ cinescout/errors.py
@@ -1,27 +1,32 @@
-import requests
+"""Handles http errors and raised exceptions."""
 
+import requests
 from flask import render_template
+
 from cinescout import app, db
 
 @app.errorhandler(404)
 def not_found_error(e):
+	"""Page not found (e.g. bad movie id)."""
 	return render_template("errors/404.html"), 404
 
-
 @app.errorhandler(401)
-def not_found_error(e):
+def not_authorized(e):
+	"""Not authorized to see page (e.g. not logged in)."""
 	return render_template("errors/401.html"), 401
 
 @app.errorhandler(429)
-def not_found_error(e):
+def too_many_requests_error(e):
+	"""Too many requests made to API in short period,
+	mostly like to NYT movie reviews."""
     err_message = ("Too many requests in a row. Please wait 30–60 seconds " \
     "before your next query.")
     return render_template("errors/429.html", err_message=err_message), 429
 
 @app.errorhandler(requests.exceptions.ConnectionError)
 def connection_error(e):
+	"""Connection error; mostly likely raised if internet is down."""
     return render_template("errors/connection-error.html")
-
 
 
 # THIS CODE WILL SUPERSEDE INTERACTIVE DEBUGGER DURING DEVELOPMENT

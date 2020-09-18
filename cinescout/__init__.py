03456144defbe3ea6036a8f7d91107f0b04a32e6
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-09-18 16:27:45 -0400

    Initially setup unsecured Flask-Admin panel

--- cinescout/__init__.py
+++ cinescout/__init__.py
@@ -3,6 +3,7 @@ can import. File executed when package imported."""
 
 print("***Executing __init__.py...***")
 
+# Import PyPi modules.
 from flask import Flask
 print("Flask successfully imported.")
 
@@ -19,6 +20,14 @@ print("LoginManager imported.")
 from flask_wtf.csrf import CSRFProtect
 print("Flask_wtf CSRFProtect imported.")
 
+from flask_admin import Admin
+print("Flask_Admin imported.")
+
+from flask_admin.contrib.sqla import ModelView
+print("Flask_Admin ModelView imported.")
+
+
+# Initialize and configure objects.
 app = Flask(__name__)
 print("app object created.")
 
@@ -39,6 +48,11 @@ csrf = CSRFProtect()
 csrf.init_app(app)
 print("CSRF object created and applied to app.")
 
+admin = Admin()
+admin.init_app(app)
+print("FlaskAdmin object created and applied to app.")
+
+# Import application modules.
 from cinescout import views
 print("cinescout.views imported.")
 
@@ -50,5 +64,12 @@ print("cinescout.errors imported.")
 
 from cinescout import movies
 print("cinescout.movies imported.")
+
+# More configuration...
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Film, db.session))
+admin.add_view(ModelView(models.CriterionFilm, db.session))
+admin.add_view(ModelView(models.PersonalFilm, db.session))
+admin.add_view(ModelView(models.FilmListItem, db.session))
 
 print("***End of __init__.py***")

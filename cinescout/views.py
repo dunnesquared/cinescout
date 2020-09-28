8675adb13c695661c9d983d2fdc829a372716c73
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2020-09-28 16:22:10 -0400

    Remove registration feature

--- cinescout/views.py
+++ cinescout/views.py
@@ -34,28 +34,28 @@ def index():
     """Home page."""
     return render_template("index.html")
 
+# Disabled as of version 1.1.0
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     """Allows non-logged in users to register for account."""
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    """Allows non-logged in users to register for account."""
+#     # Logged in users shouldn't be allowed to register.
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
 
-    # Logged in users shouldn't be allowed to register.
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
+#     form = RegistrationForm()
 
-    form = RegistrationForm()
+#     # Ensure user-entered data is good. If so, register and take to login page!
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data)
+#         user.set_password(form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash(f'Welcome to Cinescout, {form.username.data}! Now login to get started.', 'success')
+#         return redirect(url_for('login'))
 
-    # Ensure user-entered data is good. If so, register and take to login page!
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash(f'Welcome to Cinescout, {form.username.data}! Now login to get started.', 'success')
-        return redirect(url_for('login'))
-
-    # When users GET the register page
-    return render_template('register.html', title='Register', form=form)
+#     # When users GET the register page
+#     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])

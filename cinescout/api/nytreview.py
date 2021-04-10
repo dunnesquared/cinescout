1f66c0af5b6377c3a1594bf9501a3cb81d6d1d23
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-04-10 12:29:48 -0400

    Implement API function to fetch NYT movie review

--- cinescout/api/nytreview.py
+++ cinescout/api/nytreview.py
@@ -1,5 +1,9 @@
 """API to fetch NYT film review films."""
 
+from typing import Dict
+
+from datetime import datetime
+
 from flask import jsonify, request
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -7,9 +11,23 @@ from cinescout.movies import Movie
 from cinescout.reviews import NytMovieReview
 from cinescout.api import bp
 
-@bp.route("/nyt-movie-review/<string:title>")
-def get_nyt_movie_review(title):
-    """Fetches movei review from NYT api
+
+def _nyt_response_error(response : Dict) -> Dict:
+     print("Error retrieving NYT review.")
+     status_code = response['status_code']
+     if status_code == 429:
+        err_message = ("Too many requests in a row. Please wait 30–60 seconds "
+                "before your next query.")
+     else:
+        err_message = f"NYT API query failed ({status_code}): {response['message']}"
+    
+     print(err_message)
+     return {'success': False, 'err_message': err_message}, status_code
+
+
+@bp.route("/nyt-movie-review/")
+def get_nyt_movie_review():
+    """Fetches movie review from NYT api
 
     Returns:
         JSON object with the following fields:
@@ -25,6 +43,7 @@ def get_nyt_movie_review(title):
             
     """
     data = request.get_json()
+    title = data.get('title')
     original_title = data.get("original_title")
     release_year = data.get("release_year")
     release_date = data.get("release_date")
@@ -34,24 +53,62 @@ def get_nyt_movie_review(title):
     print(f"Original Title: {original_title}")
     print(f"Release Year: {release_year}")
     print(f"Release Date: {release_date}")
-    
-    # Fetch movie review data (stub data for now)
-    review_text = f"'{title}' was interesting...Five bags of popcorn!"
-    critics_pick = True
-    review_warning = False
-    
-    # Error.
-    if not review_text: 
-        err_message = "Unable to get movie review."
-        return {'success': False, 'err_message': err_message}, 404
+
+    # No point in searching for review if release year DNE.
+    if not release_year:
+        err_message = "Unable to fetch review: No review year specified in payload."
+        return {'success': False, 'err_message': err_message}, 400
+
+    # No point in searching for review if movie has yet to come out.
+    today = datetime.today()
+    release_dt = datetime.strptime(release_date, '%Y-%m-%d')
+    if release_dt > today:
+        message = "No review to display: this film has yet to be released."
+        return {'success': False, 'message': message}
         
-    # All good.
+    # Create Movie object.
+    movie = Movie(title=title, release_year=release_year, release_date=release_date,
+                  original_title=original_title)
+    
+    # Fetch movie review. Try main title first.
+    print(f"Fetching NYT movie review for '{movie.title}' ({movie.release_year})...")
+    print("Making first attempt...")
+    response = NytMovieReview.get_movie_review(movie)
+    print(response)
+
+    # Handle error.
+    if response['status_code'] != 200:
+        return _nyt_response_error(response)
+
+    # See if there's a review to print.
+    review = response.get('review', None)
+    
+    # Make second attempt. NytMovieReview will try diffently this time.
+    if not review:
+        print("Making second attempt...")
+        NytMovieReview.delay_next()
+        response = NytMovieReview.get_movie_review(movie, first_try=False)
+    
+    # Handle error.
+    if response['status_code'] != 200:
+        return _nyt_response_error(response)
+
+    # No review found for specified movie despite all attempts to find one.
+    if not review: 
+        message = "No review found for this movie."
+        return {'success': False, 'message': message}
+        
+    # All good. Extract data.
+    review_text = review.text
+    critics_pick = bool(review.critics_pick)
+    review_warning = not response.get('bullseye', None)
+
     result = {
                 'success': True, 
                 'review_text': review_text, 
                 'critics_pick': critics_pick,
                 'review_warning': review_warning
-                }
+             }
 
     return jsonify(result)
 

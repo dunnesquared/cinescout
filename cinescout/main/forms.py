b2b407e4330abc3f47aa534583c4ee9a699e0b4a
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-12-04 12:27:01 -0500

    Update packages and replace deprecated module

--- cinescout/main/forms.py
+++ cinescout/main/forms.py
@@ -2,7 +2,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Regexp, Length
 from cinescout.models import User
 
